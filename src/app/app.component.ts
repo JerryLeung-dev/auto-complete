758394d5fcf81790b8e85ec125da2ba08bb60c8a
unknown <luongvidu@gmail.com>
2022-07-07 17:21:48 +1000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Country } from '@angular-material-extensions/select-country';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+export interface InfoForm {
+  country: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'auto-complete';
+export class AppComponent implements OnInit {
+  countryFormGroup: FormGroup;
+  formBuilder: FormBuilder;
+
+  constructor(formBuilder: FormBuilder) {
+    this.formBuilder = formBuilder;
+  }
+
+  ngOnInit(): void {
+    this.countryFormGroup = this.formBuilder.group({
+      country: [
+        {
+          name: 'Deutschland',
+          alpha2Code: 'DE',
+          alpha3Code: 'DEU',
+          numericCode: '276',
+        },
+      ],
+    });
+  }
+
+  onCountrySelected(country: Country) {
+    console.log(country);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectCountryModule } from '@angular-material-extensions/select-country';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    FormsModule,
+    MatSelectCountryModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
